51dd826eec08aece64f9a519a29b4b0424a2597f
Mahdiye <mahdiye.rzk@gmail.com>
2026-08-02 23:25:17 +0330

    login & register page added to project

--- bookstore/Controllers/AccountController.cs
+++ bookstore/Controllers/AccountController.cs
@@ -1,20 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bookstore.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(Login log, string Email, string Password)
+        {
+            return View();
+        }
+
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Register reg)
         {
             return View();
         }
--- bookstore/Models/Login.cs
+++ bookstore/Models/Login.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace bookstore.Models
 {
@@ -6,7 +7,16 @@ namespace bookstore.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
+
+        [Required]
+        [Display(Name ="ددددددرمز ورود")]
+        [MinLength(12)]
+        [MaxLength(18)]
         public string Password { get; set; }
+        [Required]
+        [Display(Name = "ایمیل")]
+        [MinLength(12)]
+        [MaxLength(18)]
         public string Email { get; set; }
         public bool IsAdmin { get; set; }
 
